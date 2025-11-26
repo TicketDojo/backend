359ecef5f54e69f95672e1c03af78e2d0cadeb93
dongyeop00 <ehd_duq94@naver.com>
2025-11-26 20:29:24 +0900

    first commit

---
+++ src/main/java/com/ticket/dojo/backdeepfamily/BackdeepfamilyApplication.java
@@ -0,0 +1,13 @@
+package com.ticket.dojo.backdeepfamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackdeepfamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackdeepfamilyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticket/dojo/backdeepfamily/BackdeepfamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket.dojo.backdeepfamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackdeepfamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
