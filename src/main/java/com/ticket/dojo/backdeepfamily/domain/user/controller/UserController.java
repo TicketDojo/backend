e345e41730940a219c1ed159e1c084c7e1c917b6
DongYeop <117625978+dongyeop00@users.noreply.github.com>
2025-11-30 15:17:46 +0900

    Fix : request mapping from '/seat' to '/users'
    Signed-off-by: DongYeop <117625978+dongyeop00@users.noreply.github.com>

--- src/main/java/com/ticket/dojo/backdeepfamily/domain/user/controller/UserController.java
+++ src/main/java/com/ticket/dojo/backdeepfamily/domain/user/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/seat")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 }
