f1962c3a274a28d93d25404eb9b84d346ab18ddb
woduddl1000 <woduddl1000@gmail.com>
2025-12-20 00:31:24 +0900

    Fix: 로그인, 회원가입 json 사용으로 변경

--- src/main/java/com/ticket/dojo/backdeepfamily/domain/user/controller/UserController.java
+++ src/main/java/com/ticket/dojo/backdeepfamily/domain/user/controller/UserController.java
@@ -9,21 +9,21 @@ import com.ticket.dojo.backdeepfamily.domain.user.dto.request.UserLoginRequest;
 import com.ticket.dojo.backdeepfamily.domain.user.service.UserService;
 
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-    
+
     private final UserService userService;
-    
+
     @PostMapping("")
-    public String join(UserLoginRequest request) {
+    public String join(@RequestBody UserLoginRequest request) {
         userService.join(request);
 
         return "ok";
     }
-    
+
 }
